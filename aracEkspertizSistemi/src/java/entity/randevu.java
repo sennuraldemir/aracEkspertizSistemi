d026b78b24b3921d214f0c9ebd64acd0f8beb0ca
Demet Ergulec <demet.merve00@outlook.com>
2022-04-10 03:07:40 +0300

    demet entity de ilgili nesneler oluşturuldu

---
+++ aracEkspertizSistemi/src/java/entity/randevu.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author Mustafa
+ */
+public class randevu {
+
+    private String randevu_id;
+    private String plaka_id;
+    private String tarih;
+    private String saat;
+    private String ad;
+    private String soyad;
+    
+     public randevu() {
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
+
+    public String getRandevu_id() {
+        return randevu_id;
+    }
+
+    public void setRandevu_id(String randevu_id) {
+        this.randevu_id = randevu_id;
+    }
+
+    public String getPlaka_id() {
+        return plaka_id;
+    }
+
+    public void setPlaka_id(String plaka_id) {
+        this.plaka_id = plaka_id;
+    }
+
+    public String getTarih() {
+        return tarih;
+    }
+
+    public void setTarih(String tarih) {
+        this.tarih = tarih;
+    }
+
+    public String getSaat() {
+        return saat;
+    }
+
+    public void setSaat(String saat) {
+        this.saat = saat;
+    }
+
+    /**
+     * Creates a new instance of randevuController
+     */
+   
+
+}
