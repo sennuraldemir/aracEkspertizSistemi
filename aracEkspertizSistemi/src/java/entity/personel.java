ff299de6421fcf5ea7e6cd976e86b404c21ec087
ZehraSahaplioglu <99864727+ZehraSahaplioglu@users.noreply.github.com>
2022-04-10 04:50:08 +0300

    Update personel.java
    'zehra personel entity olusturuldu'

--- aracEkspertizSistemi/src/java/entity/personel.java
+++ aracEkspertizSistemi/src/java/entity/personel.java
@@ -4,10 +4,42 @@
  */
 package entity;
 
-/**
- *
- * @author Mustafa
- */
 public class personel {
+    private int personel_id;
+    private String adi;
+    private String soyadi;
+    private int yas;
+    private String meslegi;
     
+    public int getPersonel_id(){
+        return personel_id;
+    }
+    public void setPersonel_id(int personel_id){
+        this.personel_id = personel_id;
+    }
+    public String getAdi(){
+        return adi;
+    }
+    public void setAdi(String adi){
+        this.adi = adi;
+    }
+    public String getSoyadi(){
+        return soyadi;
+    }
+    public void setSoyadi(String soyadi){
+        this.soyadi = soyadi;
+    }
+    public int getYas(){
+        return yas;
+    }
+    public void setYas(int yas){
+        this.yas = yas;
+    }
+    public String getMeslegi(){
+        return meslegi;
+    }
+    public void setMeslegi(String meslegi){
+        this.meslegi = meslegi;
+    }
+        
 }
