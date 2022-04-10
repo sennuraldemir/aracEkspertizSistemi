aa3f37c20c083132c5957cb2304dd18dcbe6c06f
ZehraSahaplioglu <99864727+ZehraSahaplioglu@users.noreply.github.com>
2022-04-10 09:12:26 +0300

    Update personelDAO.java
    'zehra personelDAO olusturuldu'

--- aracEkspertizSistemi/src/java/dao/personelDAO.java
+++ aracEkspertizSistemi/src/java/dao/personelDAO.java
@@ -1,24 +1,93 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
- */
+
 package dao;
 
-import jakarta.inject.Named;
-import jakarta.enterprise.context.Dependent;
+import entity.personel;
+import java.sql.Connection;
+import java.sql.ResulSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- *
- * @author Mustafa
- */
-@Named(value = "personelDAO")
-@Dependent
-public class personelDAO {
 
-    /**
-     * Creates a new instance of personelDAO
-     */
-    public personelDAO() {
+
+public class personelDAO extends DBConnection{
+    
+    private Connection db;
+    
+    public void create (personel c) {
+        try{
+            Statement st = this.connect().createStatement();
+            
+            String query = "insert into personel(adi, soyadi, yas, meslegi) values('"+c.getAdi() + "','"+c.getSoyadi() + "','"+c.getYas() + "','"+c.getMeslegi()+"');
+            st.executeUpdate(query);
+        
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+            
+        }
+        
     }
+    
+    public void update (personel c) {
+        try{
+            Statement st = this.connect().createStatement();
+            
+            String query = "update personel set adi='"+c.getAdi()+"', soyadi='"+c.getSoyadi()+"', yas='"+c.getYas()+"', meslegi='"+c.getMeslegi()"'where id="+c.getPersonel_id();
+            st.executeUpdate(query);
+        
+        } catch (Exception ex){
+            System.out.println(ex.getMessage());
+            
+        }
+        
+    }
+    
+    public void delete (personel c) {
+        try{
+            Statement st= this.connect().createStatement();
+            String query= "delete from category where id" + c.getPersonel_id();
+            st.executeUpdate(query);
+            
+        }catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            
+        }
+        
+    } 
+    
+    public List<personel> getList(){
+        List<personel> list = new ArrayList<>();
+        
+        try{
+            Statement st = this.connect().createStatement();
+            String query="select * from personel";
+            
+            ResultSet rs = st.executeQuery(query);
+            
+            while (rs.next()) {
+                list.add(new personel(rs.getInt("personel_id"),rs.getString("adi"),rs.getString("soyadi"),rs.getInt("yas"),rs.getString("meslegi")));
+            }
+            
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+            
+        }
+        return list;
+        
+    }
+    
+    public Connection getDb() {
+        if (this.db == null){
+            this.db = this.connect();
+        }
+        return db;
+    }
+    
+    public void setDb(Connection db){
+        this.db=db;
+        
+    }
+    
+
     
 }
