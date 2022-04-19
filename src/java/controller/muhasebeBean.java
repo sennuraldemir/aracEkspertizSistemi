b82d16f378524b76f6f6d6209b175fa935b913ef
Demet Ergüleç <77542723+demetrglc@users.noreply.github.com>
2022-04-19 15:15:30 +0300

    Add files via upload

---
+++ src/java/controller/muhasebeBean.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package controller;
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.SessionScoped;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Mustafa
+ */
+@Named(value = "muhasebeBean")
+@SessionScoped
+public class muhasebeBean implements Serializable {
+
+    /**
+     * Creates a new instance of muhasebeBean
+     */
+    public muhasebeBean() {
+    }
+    
+}
