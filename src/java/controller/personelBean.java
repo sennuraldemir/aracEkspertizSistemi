575d0f4abb2e4234a5a4b9802de6fa8b9950797e
ZehraSahaplioglu <99864727+ZehraSahaplioglu@users.noreply.github.com>
2022-04-21 13:45:15 +0300

    Update personelBean.java
    Zehra personel controller oluşturdu

--- src/java/controller/personelBean.java
+++ src/java/controller/personelBean.java
@@ -1,25 +1,78 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
- */
+
 package controller;
 
+import dao.personelDAO;
+import entity.personel;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  *
- * @author Mustafa
+ * @author ZEHRA
  */
+
 @Named(value = "personelBean")
 @SessionScoped
+
 public class personelBean implements Serializable {
 
-    /**
-     * Creates a new instance of personelBean
-     */
+    private personel entity;
+    private personelDAO dao;
+    private List<personel> list;
+    
     public personelBean() {
     }
+
+    public void create(){
+        this.getDao().create(entity);
+        entity=new personel();
+    }
     
+    public void update(){
+        this.getDao().update(entity);
+        entity=new personel();
+    }
+    
+    public void delete(personel a){
+        this.getDao().delete(a);
+        entity=new personel();
+    }
+    
+    public void clear(){
+        entity=new personel();
+    }
+    
+    public personel getEntity() {
+        if (entity == null) {
+            entity = new personel();
+        }
+        return entity;
+    }
+
+    public void setEntity(personel entity) {
+        this.entity = entity;
+    }
+
+    public personelDAO getDao() {
+        if (dao == null) {
+            dao = new personelDAO();
+        }
+        return dao;
+    }
+
+    public void setDao(personelDAO dao) {
+        this.dao = dao;
+    }
+
+    public List<personel> getList() {
+        this.list = this.getDao().getList();
+        return list;
+    }
+
+    public void setList(List<personel> list) {
+        this.list = list;
+    }
+
 }
