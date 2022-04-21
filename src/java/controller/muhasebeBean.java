ffcc3f153293724ba072a0f563ed89eb1025b090
ZehraSahaplioglu <99864727+ZehraSahaplioglu@users.noreply.github.com>
2022-04-21 13:28:55 +0300

    Update muhasebeBean.java
    Zehra muhasebe controller tamamladı

--- src/java/controller/muhasebeBean.java
+++ src/java/controller/muhasebeBean.java
@@ -1,25 +1,77 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
- */
+
 package controller;
 
+import dao.muhasebeDAO;
+import entity.muhasebe;
 import jakarta.inject.Named;
 import jakarta.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
 
 /**
  *
- * @author Mustafa
+ * @author ZEHRA
  */
+
 @Named(value = "muhasebeBean")
 @SessionScoped
-public class muhasebeBean implements Serializable {
 
-    /**
-     * Creates a new instance of muhasebeBean
-     */
+public class muhasebeBean implements Serializable {
+    
+    private muhasebe entity;
+    private muhasebeDAO dao;
+    private List<muhasebe> list;
+    
     public muhasebeBean() {
     }
     
+    public void create(){
+        this.getDao().create(entity);
+        entity = new muhasebe();
+    }
+    
+    public void update(){
+        this.getDao().update(entity);
+        entity = new muhasebe();
+    }
+    
+    public void delete(muhasebe m){
+        this.getDao().delete(m);
+        entity = new muhasebe();
+    }
+    
+    public void clear(){
+        entity = new muhasebe();
+    }
+    
+    public muhasebe getEntity(){
+        if (entity == null){
+            entity = new muhasebe();
+        }
+        return entity;
+    }
+    
+    public void setEntity(muhasebe entity){
+        this.entity = entity;
+    }
+    
+    public muhasebeDAO getDAO(){
+        if (dao == null){
+            dao = new muhasebeDAO();
+        }
+        return dao;
+    }
+    
+    public void setDao(muhasebeDAO dao){
+        this.dao = dao;
+    }
+    
+    public List<muhasebe> getList(){
+        this.list = this.getDao().getList();
+        return list;
+    }
+    public void setList(List<muhasebe> list){
+        this.list = list;
+    }
+        
 }
