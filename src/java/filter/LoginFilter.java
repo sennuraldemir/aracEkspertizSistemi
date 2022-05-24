e5e067f7547acb8466fce62a8d6491af845127a7
Sennur Aldemir <snnrldmr@gmail.com>
2022-05-24 10:52:12 +0300

    sennur login oluşturuldu.

---
+++ src/java/filter/LoginFilter.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filter;
+
+import entity.UserManager;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
+
+/**
+ *
+ * @author gurkangltekin
+ */
+@WebFilter("/*") //Bütün url yi dinle
+public class LoginFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest sr, ServletResponse sr1, FilterChain fc) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) sr;
+        HttpServletResponse response = (HttpServletResponse) sr1;
+
+        String url = request.getRequestURI();
+
+        HttpSession session = request.getSession();
+
+        UserManager user = null;
+
+        if (session != null) {
+            user = (UserManager) session.getAttribute("validUser");
+
+        }
+        if (user == null) {
+
+            if (url.contains("logout") || url.contains("private")) {
+                response.sendRedirect(request.getContextPath() + "/login.xhtml");
+            } else {
+                fc.doFilter(sr, sr1);
+            }
+        } else {
+            if (url.contains("register")) {
+                response.sendRedirect(request.getContextPath() + "/index.xhtml");
+            } else if (url.contains("logout")) {
+                session.invalidate();
+                response.sendRedirect(request.getContextPath() + "/login.xhtml");
+            } else {
+                fc.doFilter(sr, sr1);
+            }
+        }
+
+    }
+
+}
