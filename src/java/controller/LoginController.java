bb74047c36b768bd55eeedfc1588cd8c74d2c941
Demet Ergüleç <77542723+demetrglc@users.noreply.github.com>
2022-05-26 10:57:20 +0300

    Update LoginController.java
    demet logincontroller

--- src/java/controller/LoginController.java
+++ src/java/controller/LoginController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package controller;
+package bean;
 
 import entity.UserManager;
 import jakarta.enterprise.context.SessionScoped;
@@ -46,3 +46,4 @@ public class LoginController implements Serializable{
     
     
 }
+
