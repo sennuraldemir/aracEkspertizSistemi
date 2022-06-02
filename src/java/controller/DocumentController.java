31ae206defed8511b5c76da62457a1a3bc9ad40e
sennuraldemir <99283497+sennuraldemir@users.noreply.github.com>
2022-06-02 13:03:01 +0300

    Add files via upload
    dosyalama eklendi bean

--- src/java/controller/DocumentController.java
+++ src/java/controller/DocumentController.java
@@ -1,44 +1,68 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
  */
 package controller;
 
 import dao.DocumentDAO;
 import entity.Document;
-import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
-import jakarta.mail.Part;
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.servlet.http.Part;
 import java.io.File;
 import java.io.InputStream;
 import java.io.Serializable;
 import java.nio.file.Files;
 import java.util.List;
 
-
-@Named
+/**
+ *
+ * @author Mustafa
+ */
+@Named(value = "documentController")
 @SessionScoped
-public class DocumentController implements Serializable{
+public class DocumentController implements Serializable {
+
+    /**
+     * Creates a new instance of DocumentController
+     */
+    
     private Document document;
     private List<Document> documentList;
     private DocumentDAO documentDao;
-    
     private Part doc;
-     private final String uploadTo= "/Users/Sennur/upload/";
-     
-     public void upload(){
-         try{
-             InputStream input = doc.getInputStream();
-             File f= new File(uploadTo+doc.getSubmittedFileName());
-             Files.copy(input,f.toPath());
-         }catch(Exception e){
-             System.out.println(e.getMessage());
-         }
-     }
+    
+    private final String uploadTo ="/Users/Sennur/upload";
+    
+    public DocumentController() {
+    }
+    
+    public void upload(){
+        try{
+            InputStream input =doc.getInputStream();
+            File f =new File(uploadTo+doc.getSubmittedFileName());
+            Files.copy(input, f.toPath());
+            
+            document =this.getDocument();
+            document.setFilePath(f.getParent());
+            document.setFileName(f.getName());
+            document.setFileType(doc.getContentType());
+            
+            this.getDocumentDao().create(document);
+            
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 
+    public String getUploadTo() {
+        return uploadTo;
+    }
+    
     public Document getDocument() {
-        if(this.document == null)
-            this.document = new Document();
+        if(this.document==null){
+            this.document=new Document();
+        }
         return document;
     }
 
@@ -47,7 +71,7 @@ public class DocumentController implements Serializable{
     }
 
     public List<Document> getDocumentList() {
-        this.documentList=this.getDocumentDao().findAll();
+        this.documentList=this.getDocumetDao().findAll();
         return documentList;
     }
 
@@ -55,9 +79,18 @@ public class DocumentController implements Serializable{
         this.documentList = documentList;
     }
 
+    public DocumentDAO getDocumetDao() {
+        if(this.documentDao==null){
+            this.documentDao=new DocumentDAO();
+        }
+        return documentDao;
+    }
+
+    public void setDocumetDao(DocumentDAO documetDao) {
+        this.documentDao = documetDao;
+    }
+
     public DocumentDAO getDocumentDao() {
-        if(this.documentDao == null)
-            this.documentDao = new DocumentDAO();
         return documentDao;
     }
 
@@ -72,10 +105,5 @@ public class DocumentController implements Serializable{
     public void setDoc(Part doc) {
         this.doc = doc;
     }
-
-    public String getUploadTo() {
-        return uploadTo;
-    }
-    
     
 }
