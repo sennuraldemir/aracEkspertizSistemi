798cb1b9b8f77ba43ef4d034df8c31b63efb1619
aylin1453 <101657113+aylin1453@users.noreply.github.com>
2022-06-02 13:09:12 +0300

    Update MemnuniyetRaporuDAO.java
    memnuniyet dao guncellendi

--- src/java/dao/MemnuniyetRaporuDAO.java
+++ src/java/dao/MemnuniyetRaporuDAO.java
@@ -1,22 +1,23 @@
 package dao;
 
 import entity.MemnuniyetRaporu;
-import util.DBConnection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import util.DBConnection;
 
 public class MemnuniyetRaporuDAO extends DBConnection {
 
- 
+    public MemnuniyetRaporuDAO() {
+    }
+    
 
-    public void create(MemnuniyetRaporu c) {
+    public void create(MemnuniyetRaporu m) {
 
         try {
             Statement st = this.getConnection().createStatement();
-
-            String query = "insert into memnuniyetRaporu (firma_adi, begenmedim,kismi_begendim, yeterli,begendim) values ('" + c.getFirma_adi() + "','" + c.getBegenmedim() + "','" + c.getKismi_begendim() + "','" + c.getYeterli() + "','" + c.getBegendim() + "')";
+            String query = "insert into memnuniyet_raporu (memnuniyet_id,firma_adi, begenmedim, kismi_begendim,yeterli, begendim) values ('" + m.getMemnuniyet_id() + "','" + m.getFirma_adi() + "','" + m.getBegenmedim() + "','" + m.getKismi_begendim() + "','" + m.getYeterli() + "','" + m.getBegendim() + "')";
             st.executeUpdate(query);
 
         } catch (Exception ex) {
@@ -24,10 +25,10 @@ public class MemnuniyetRaporuDAO extends DBConnection {
         }
     }
 
-    public void update(MemnuniyetRaporu c) {
+    public void update(MemnuniyetRaporu m) {
         try {
             Statement st = this.getConnection().createStatement();
-            String query = "update memnuniyetRaporu set firma_adi='" + c.getFirma_adi() + "', begenmedim = '" + c.getBegenmedim() + "', kismi_begendim = '" + c.getKismi_begendim() + "',yeterli='" + c.getYeterli() + "', begendim = '" + c.getBegendim() + "' where odeme_id= " + c.getMemnuniyet_id();
+            String query = "update memnuniyet_raporu set firma_adi='" + m.getFirma_adi() + "', begenmedim = '" + m.getBegenmedim() + "', kismi_begendim = '" + m.getKismi_begendim() + "',yeterli='" + m.getYeterli() + "', begendim = '" + m.getBegendim() + "' where odeme_id= " + m.getMemnuniyet_id() + "'";
 
             st.executeUpdate(query);
 
@@ -36,11 +37,10 @@ public class MemnuniyetRaporuDAO extends DBConnection {
         }
     }
 
-    public void delete(MemnuniyetRaporu c) {
+    public void delete(MemnuniyetRaporu m) {
         try {
             Statement st = this.getConnection().createStatement();
-
-            String query = "delete from memnuniyetRaporu where memnuniyet_id = " + c.getMemnuniyet_id();
+            String query = "delete from memnuniyet_raporu where memnuniyet_id = " + m.getMemnuniyet_id();
             st.executeUpdate(query);
 
         } catch (Exception ex) {
@@ -49,16 +49,12 @@ public class MemnuniyetRaporuDAO extends DBConnection {
 
     }
 
-    public List<MemnuniyetRaporu> getList() {
+    public List<MemnuniyetRaporu> readList() {
         List<MemnuniyetRaporu> list = new ArrayList<>();
 
         try {
             Statement st = this.getConnection().createStatement();
-            String query = "select * from menuniyetRaporu";
-
-            int r = st.executeUpdate(query);
-
-            ResultSet rs = st.executeQuery(query);
+            ResultSet rs = st.executeQuery("select * from memnuniyet_raporu");
 
             while (rs.next()) {
                 list.add(new MemnuniyetRaporu(rs.getInt("memnuniyet_id"), rs.getString("firma_adi"), rs.getString("begenmedim"), rs.getString("kismi_begendim"), rs.getString("yeterli"), rs.getString("begendim")));
@@ -66,6 +62,22 @@ public class MemnuniyetRaporuDAO extends DBConnection {
 
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
+        }
+        return list;
+    }
+     public List<MemnuniyetRaporu> readList(int page) {
+        int offset = (page - 1) * 5;
+        List<MemnuniyetRaporu> list = new ArrayList<>();
+
+        try {
+            Statement st = this.getConnection().createStatement();
+             ResultSet rs = st.executeQuery("select*from memnuniyet_raporu memnuniyet_id limit 5 offset " + offset);
+            while (rs.next()) {
+                  list.add(new MemnuniyetRaporu(rs.getInt("memnuniyet_id"), rs.getString("firma_adi"), rs.getString("begenmedim"), rs.getString("kismi_begendim"), rs.getString("yeterli"), rs.getString("begendim")));
+           
+                }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
         return list;
     }
