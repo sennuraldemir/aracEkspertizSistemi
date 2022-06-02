22c43f137ab7ae7ba2b66ef93e7be5fb033b1779
aylin1453 <101657113+aylin1453@users.noreply.github.com>
2022-06-02 13:05:58 +0300

    Update denetimBean.java
    denetim bean guncellendi

--- src/java/controller/denetimBean.java
+++ src/java/controller/denetimBean.java
@@ -7,40 +7,40 @@ import jakarta.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import java.util.List;
 
-@Named(value = "denetimBean")
+@Named(value="denetimBean")
 @SessionScoped
 
-public class denetimBean implements Serializable {
+public class DenetimBean implements Serializable {
 
     private Denetim entity;
     private DenetimDAO dao;
     private List<Denetim> list;
+    
+    private int page=1;
+    private int pageCount=0;
 
-    public denetimBean() {
+    public void clearForm() {
+        this.entity = new Denetim();
     }
 
-    public void create(){
+    public void create() {
         this.getDao().create(entity);
-        entity=new Denetim();
+        this.entity = new Denetim();
     }
-    
-    public void update(){
+
+    public void update() {
         this.getDao().update(entity);
-        entity=new Denetim();
+        this.entity = new Denetim();
     }
-    
-    public void delete(Denetim a){
-        this.getDao().delete(a);
-        entity=new Denetim();
+
+    public void delete() {
+        this.getDao().delete(entity);
+        this.entity = new Denetim();
     }
-    
-    public void clear(){
-        entity=new Denetim();
-    }
-    
+
     public Denetim getEntity() {
-        if (entity == null) {
-            entity = new Denetim();
+        if (this.entity == null) {
+            this.entity = new Denetim();
         }
         return entity;
     }
@@ -50,8 +50,8 @@ public class denetimBean implements Serializable {
     }
 
     public DenetimDAO getDao() {
-        if (dao == null) {
-            dao = new DenetimDAO();
+        if (this.dao == null) {
+            this.dao = new DenetimDAO();
         }
         return dao;
     }
@@ -59,14 +59,49 @@ public class denetimBean implements Serializable {
     public void setDao(DenetimDAO dao) {
         this.dao = dao;
     }
+    
+    public void previous(){
+        page--;
+        if(page<1){
+            page=this.getPageCount();
+        }
+    }
+    public void next(){
+        page++;
+        if(page>this.getPageCount()){
+            page=1;
+        }
+    }
+    
 
     public List<Denetim> getList() {
-        this.list = this.getDao().getList();
+        this.list = this.getDao().readList(page);
         return list;
     }
 
     public void setList(List<Denetim> list) {
         this.list = list;
     }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPageCount() {
+        List<Denetim> glist=this.getDao().readList();
+        int size = glist.size();
+        pageCount=(int) Math.ceil(size/5);
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+    
+    
 
 }
